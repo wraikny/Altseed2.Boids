0ffbcf4ad22b1fdefbf54895affc768b872024eb
wraikny <writtenknown@gmail.com>
2020-08-03 18:41:55 +0900

    initialize

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Altseed2.Boids
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
